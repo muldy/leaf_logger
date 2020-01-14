ef07970590068fe9c339ed0972932005bdfbeb19
muldy <muldy@nuc.lan>
2020-01-14 09:48:20 +0000

    log to file

--- server.py
+++ server.py
@@ -1,10 +1,21 @@
+import logging
+import logging.config
+from logging.handlers import TimedRotatingFileHandler
 from flask import Flask
 from flask_request_logger import RequestLogger
+import sqlalchemy
 
-app = Flask('hello')
+path ="access.log"
+
+logger = logging.getLogger('werkzeug')
+handler = TimedRotatingFileHandler(path, when="midnight")
+logger.addHandler(handler)
+
+app = Flask('leaf logger')
 req_logger = RequestLogger(app)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def hello():
-    return 'hello'
+    return 'leaf logger'
+
